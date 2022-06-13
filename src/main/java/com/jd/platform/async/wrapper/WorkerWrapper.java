a8f65100aa0663b6f49cfe622edc38d61f85f506
wuweifeng10 <wuweifeng10@jd.com>
2022-06-13 10:01:50 +0800

    格式

--- src/main/java/com/jd/platform/async/wrapper/WorkerWrapper.java
+++ src/main/java/com/jd/platform/async/wrapper/WorkerWrapper.java
@@ -218,7 +218,7 @@ public class WorkerWrapper<T, V> {
     private synchronized void doDependsJobs(ExecutorService executorService, List<DependWrapper> dependWrappers, WorkerWrapper fromWrapper, long now, long remainTime) {
         //如果当前任务已经完成了，依赖的其他任务拿到锁再进来时，不需要执行下面的逻辑了。
         if (getState() != INIT) {
-            return ;
+            return;
         }
         boolean nowDependIsMust = false;
         //创建必须完成的上游wrapper集合
